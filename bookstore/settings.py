a13cad1519e0c293b8c9620ecb43424c489e41e2
RParker505 <rchiado75@gmail.com>
2024-10-16 16:10:38 -0600

    Update Allowed Hosts to correct URL

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['radiant-inlet-59577.herokuapp.com/']
+ALLOWED_HOSTS = ['radiant-inlet-59577-4ea82c23087c.herokuapp.com/']
 
 
 # Application definition
