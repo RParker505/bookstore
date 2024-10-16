66bb475982e48f0f7ea68db948c7f3b1d1d62e08
RParker505 <rchiado75@gmail.com>
2024-10-16 16:18:47 -0600

    Try again with Allowed Hosts

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://radiant-inlet-59577-4ea82c23087c.herokuapp.com/']
+ALLOWED_HOSTS = ['radiant-inlet-59577-4ea82c23087c.herokuapp.com']
 
 
 # Application definition
